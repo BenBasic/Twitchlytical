eb795750474e8c992fcc55d7ef3037eea2ed8367
R a b i a <rabia.saeed.1995@gmail.com>
2022-10-07 20:11:26 -0400

    graphql now works

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ if (process.env.NODE_ENV === "production") {
 const startApolloServer = async (typeDefs, resolvers) => {
 	await server.start();
 
+	server.applyMiddleware({ app });
+
 	db.once("open", () => {
 		app.listen(PORT, () => {
 			console.log(`API server running on port ${PORT}!`);
