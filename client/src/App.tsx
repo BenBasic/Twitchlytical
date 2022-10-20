7b36f7050762cc9c1af5c9f1f8c70504ba80294c
BenBasic <benjaminmbasic@gmail.com>
2022-10-20 19:53:05 -0400

    Created TotalData component, began implementing and learning D3, currently have a very basic bar graph with test data and adding/removing bars with data. Installed D3 related packages and types for use with TypeScript. Will continue looking into D3 to make things more functional/look better

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import TopStats from './components/TopStats';
+import TopData from './components/TotalData';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <TopData />
     <TopStats />
+    </>
   );
 }
 
