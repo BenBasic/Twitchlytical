eb1112918e33bbaa6606a305529955abbaa2db3d
BenBasic <benjaminmbasic@gmail.com>
2022-10-24 23:00:15 -0400

    Added second visualized dataset to AreaChart component, will now display 2 different colored datasets with their own tooltips and values. Currently only works with 2 sets of data, and can only be used with current hard coded keys (count and channels), will refactor code to be more modular later

--- client/src/components/AreaChart.tsx
+++ client/src/components/AreaChart.tsx
@@ -13,11 +13,11 @@ import * as d3 from 'd3'
 const now = new Date();
 
 const data = [
-    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 5), count: 25 },
-    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 4), count: 30 },
-    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 3), count: 75 },
-    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 2), count: 57 },
-    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1), count: 68 },
+    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 5), count: 25, channel: 10 },
+    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 4), count: 30, channel: 8 },
+    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 3), count: 75, channel: 38 },
+    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 2), count: 57, channel: 45 },
+    { year: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1), count: 68, channel: 30 },
 ];
 
 const percentDifference = (a: number, b: number) => {
@@ -55,7 +55,7 @@ const AreaChart: React.FC = () => {
 
     const yAxis = axisLeft(y)
 
-    const xAxis = axisBottom(x).ticks(5)
+    const xAxis = axisBottom(x).ticks(5).tickPadding(12)
 
     const areaRef: any = area()
     .x((d:any)=> x(d.year))
@@ -67,79 +67,96 @@ const AreaChart: React.FC = () => {
     .y0(y(0))
     .y1(dimensions.height - 100)
 
+    const channelAreaRef: any = area()
+    .x((d:any)=> x(d.year))
+    .y0(y(0))
+    .y1((d:any)=> y(d.channel))
+
 
     useEffect(() => {
         if (!selectionState) {
             setSelectionState(select(areaChart.current))
         } else {
 
-            const xAxisGroup = selectionState
-                .append('g')
-                .attr('transform', `translate(20, ${dimensions.height - 50})`)
-                .call(xAxis)
 
-            const yAxisGroup = selectionState
-                .append('g')
-                .attr('transform', `translate(20, 50)`)
-                .call(yAxis)
 
             // Creating a tooltip, default state is to have 0 opacity
             const tooltip = d3
                 .select('.areaChart')
                 .append('div')
                 .attr('class', 'tooltip')
-                .style("opacity", 0)
+                .style("opacity", 0);
 
             // Creating a gradient to be referenced as an area fill
             let gradient = selectionState
-                .append("defs")
-                .append("linearGradient")
-                .attr("id","gradient")
-                .attr('gradientTransform', 'rotate(90)')
-            
-            // Assigning starting color of gradient
-            gradient
-                .append("stop")
-                .attr("stop-color","#7D3AC1")
-                .attr("offset","0%")
-            
-            // Assigning ending color of gradient
-            gradient
-                .append("stop")
-                .attr("stop-color","#AF4BCE")
-                .attr("offset","100%");
+                .append("defs");
 
-
-
-            selectionState
+            let dataVisuals = selectionState
                 .attr('width', dimensions.width)
                 .attr('height', dimensions.height)
-                .style('background-color', '#4c6485')
-                .append('path')
-                .datum(dataState)
-                .attr('d', startAreaRef)
-                .attr('transform', 'translate(20, 50)')
-                .attr('fill', 'url(#gradient)')
-                .attr('stroke', '#29066B')
-                .attr('stroke-width', 2)
-                .transition()
-                .duration(1000)
-                .ease(easeElastic)
+                .style('background-color', '#4c6485');
 
 
-                .attr('d', areaRef)
+            for (let i = 0; i < 2; i++) {
+                const newG = gradient
+                    .append("linearGradient")
+                    .attr("id",`gradient${i}`)
+                    .attr('gradientTransform', 'rotate(90)');
+
+                // Assigning starting color of gradient
+                newG
+                    .append("stop")
+                    .attr("stop-color", i === 0 ? "#7D3AC1" : "#EB548C")
+                    .attr("offset","0%");
+        
+                // Assigning ending color of gradient
+                newG
+                    .append("stop")
+                    .attr("stop-color", i === 0 ? "#AF4BCE" : "#EA7369")
+                    .attr("offset","100%");
+
+                dataVisuals
+                    .append('path')
+                    .datum(dataState)
+                    .attr('d', startAreaRef)
+                    .attr('transform', 'translate(50, 50)')
+                    .attr('fill', i === 0 ? 'url(#gradient0)' : 'url(#gradient1)')
+                    .attr('stroke', i === 0 ? '#29066B' : '#A5194D')
+                    .attr('stroke-width', '.25rem')
+
+                    .transition()
+                    .duration(1000)
+                    .ease(easeElastic)
+
+                    .attr('d', i === 0 ? areaRef : channelAreaRef);
+            };
+            
 
 
+            const xAxisGroup = selectionState
+                .append('g')
+                .attr('transform', `translate(50, ${dimensions.height - 50})`)
+                .style('stroke-width', '.3rem')
+                .style("font-size", "1rem")
+                .call(xAxis);
 
-            selectionState
+            const yAxisGroup = selectionState
+                .append('g')
+                .attr('transform', `translate(50, 50)`)
+                .style('stroke-width', '0rem')
+                .call(yAxis);
+
+
+            for (let tIndex = 0; tIndex < 2; tIndex++) {
+                selectionState
                 .selectAll("dot")
                 .data(dataState)
                 .enter()
                 .append("circle")
-                .attr("r", 5)
+                .attr("r", 15)
                 .attr("cx", function(d) { return x(d.year); })
-                .attr("cy", function(d) { return y(d.count); })
-                .attr('transform', 'translate(20, 50)')
+                .attr("cy", function(d) { return y( tIndex === 0 ? d.count : d.channel ); })
+                .attr('transform', 'translate(50, 50)')
                 .on("mouseover", function(event, d) {
                     // Assigning i to find the index of matching data (this functions as the index)
                     const i: number = dataState.indexOf(d);
@@ -156,7 +173,7 @@ const AreaChart: React.FC = () => {
                     Color and unicode sign will be assigned based on if current data is higher
                     or lower than the previous data
                     */
-                    if (i > 0) {
+                    if (i > 0 && tIndex === 0) {
                         if (d.count > dataState[i - 1].count) {
                             greatCheck = `▲ ${percentDifference(dataState[i - 1].count, d.count)}%`
                             colorClass = `higher`
@@ -164,14 +181,22 @@ const AreaChart: React.FC = () => {
                             greatCheck = `▼ ${percentDifference(dataState[i - 1].count, d.count)}%`
                             colorClass = `lower`
                         };
-                    };
+                    } else if (i > 0 && tIndex === 1) {
+                        if (d.channel > dataState[i - 1].channel) {
+                            greatCheck = `▲ ${percentDifference(dataState[i - 1].channel, d.channel)}%`
+                            colorClass = `higher`
+                        } else {
+                            greatCheck = `▼ ${percentDifference(dataState[i - 1].channel, d.channel)}%`
+                            colorClass = `lower`
+                        };
+                    }
 
                     tooltip.transition()
                         .duration(200)
                         .style("opacity", .9);
                     tooltip.html(
                         `<p> Date: ${(d.year).toDateString()}</p>` +
-                        `<p> Count: ${d.count}</p>` +
+                        `<p>${ tIndex === 0 ? 'Count: ' + d.count : 'Channels: ' + d.channel }</p>` +
                         `<p class=${colorClass}>${greatCheck}</p>`
                         )
                         .style("left", (event.pageX + 10) + "px")
@@ -182,10 +207,14 @@ const AreaChart: React.FC = () => {
                         .duration(500)
                         .style("opacity", 0);
                 });
+            };
+
+
+
                 
 
-        }
-    })
+        };
+    });
 
 
     return (
