6e0143a654af85a355062e289ed0cf6ea4fd6f13
BenBasic <benjaminmbasic@gmail.com>
2023-01-03 19:55:40 -0500

    TopClips component lazy loads if not on screen

--- client/src/components/TopClips.tsx
+++ client/src/components/TopClips.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import ClipCard from './ClipCard';
 // import { indigo, deepPurple } from '@mui/material/colors';
 import indigo from '@mui/material/colors/indigo';
-import { ClipCollectionProps } from './TypesAndInterfaces'
+import { ClipCollectionProps } from './TypesAndInterfaces';
+import { InView } from 'react-intersection-observer';
 
 const styles = {
     container: {
@@ -26,52 +27,55 @@ const styles = {
 
 const TopClips: React.FC<ClipCollectionProps> = (props) => {
 
-    // const { loading, data, error } = useQuery(GET_TOP_CLIPS_WEEK);
+    const [isInView, setIsInView] = useState<boolean>(false);
+    console.log("isInView: " + isInView)
 
-    // const topClipData = data?.getTopClips?.[0]?.topClips;
-
-    // console.log("TOP CLIP DATA -----------------------------")
-    // console.log(topClipData)
+    // `${isInView}`
 
     return (
 
-
-        <Grid container alignItems="center" justifyContent="center">
-            <Grid container spacing={0} m={0} maxWidth="md" justifyContent="center" style={styles.container}>
-                <Grid item xs={12} textAlign="center">
-                    <Typography variant={'h4'} mt={2} mb={1} textAlign='center'
-                        style={styles.mainTitle}
-                        fontSize={{ xs: '1.85rem', sm: '2.13rem' }}
-                    >
-                        {props.home === true ? "Top Clips" : "All Time Top Clips"}
-                    </Typography>
-                </Grid>
-
-                {props.loading === false ?
-                    props.data.map((clip: any, index: any) => (
-                        <Grid item xs={ index === 0 ? 10 : 4} sm={2.4} py={2} px={1} key={index}>
-                            <ClipCard
-                                key={index}
-                                title={clip.title}
-                                broadcasterName={clip.broadcaster_name}
-                                url={clip.embed_url}
-                                thumbnail={clip.thumbnail_url}
-                                createdAt={clip.created_at}
-                                views={clip.view_count}
-                                home={props.home}
-                            />
-                        </Grid>
-
-                    )) :
-                    <Grid item xs={12}>
-                        <Typography variant={'h4'} textAlign='center'>
-                            Loading Clips...
+        <InView as="div" triggerOnce={true}
+        onChange={(inView, entry) => {
+            setIsInView(inView)
+            console.log("isInView2: " + isInView)
+        }}
+        >
+            <Grid container alignItems="center" justifyContent="center">
+                <Grid container spacing={0} m={0} maxWidth="md" justifyContent="center" style={styles.container}>
+                    <Grid item xs={12} textAlign="center">
+                        <Typography variant={'h4'} mt={2} mb={1} textAlign='center'
+                            style={styles.mainTitle}
+                            fontSize={{ xs: '1.85rem', sm: '2.13rem' }}
+                        >
+                            {props.home === true ? "Top Clips" : "All Time Top Clips"}
                         </Typography>
                     </Grid>
-                }
-            </Grid>
-        </Grid>
 
+                    {props.loading === false && isInView === true ?
+                        props.data.map((clip: any, index: any) => (
+                            <Grid item xs={index === 0 ? 10 : 4} sm={2.4} py={2} px={1} key={index}>
+                                <ClipCard
+                                    key={index}
+                                    title={clip.title}
+                                    broadcasterName={clip.broadcaster_name}
+                                    url={clip.embed_url}
+                                    thumbnail={clip.thumbnail_url}
+                                    createdAt={clip.created_at}
+                                    views={clip.view_count}
+                                    home={props.home}
+                                />
+                            </Grid>
+
+                        )) :
+                        <Grid item xs={12}>
+                            <Typography variant={'h4'} textAlign='center'>
+                                Loading Clips...
+                            </Typography>
+                        </Grid>
+                    }
+                </Grid>
+            </Grid>
+        </InView>
     )
 }
 
